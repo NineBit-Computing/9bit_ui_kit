46e42149b5f0f56b83768d17588d8436c69f0526
pragati-9bit <155516616+pragati-9bit@users.noreply.github.com>
2024-02-19 12:17:02 +0530

    fix: work on reviewing comments.replace document with querySelector

--- src/js/components/imageBrowser/imageBrowser.js
+++ src/js/components/imageBrowser/imageBrowser.js
@@ -2,9 +2,9 @@
  * Custom element definition for CardComponent
  */
 class ImageBrowserComponent extends HTMLElement {
-  /**
-   * constructor
-   */
+/**
+ * constructor
+ */
   constructor() {
     super();
     this.options = JSON.parse(this.dataset.options);
@@ -15,51 +15,47 @@ class ImageBrowserComponent extends HTMLElement {
    */
   connectedCallback() {
     const { imgSrc, imgSrc1 } = this.options;
+
+    // Include Bootstrap CSS within the component
     this.innerHTML = `
-        <div class="container mt-4 mb-4">
+    
+      <div class="container mt-4 mb-4">
         <div class="row">
           <div class="col-md-3">
             <h5>Related Media</h5>
-            <div id="relatedMedia">
-              <!-- Related media will be appended here dynamically -->
-            </div>
+            <div class="related-media"></div>
           </div>
           <div class="col-md-9">
-            <img
-              id="mainImage"
-              src=${imgSrc1}"
-              class="img-fluid mb-3"
-            />
+            <img class="main-image img-fluid mb-3" src="${imgSrc1}">
           </div>
         </div>
       </div>
-        `;
-    // Function to load related media
-    /**
-     * loadRelatedMedia
-     */
-    function loadRelatedMedia() {
-      const relatedMediaContainer = document.getElementById('relatedMedia');
-      imgSrc.forEach((mediaURL) => {
-        const imgContainer = document.createElement('div');
-        imgContainer.classList.add('mb-2');
-        const img = document.createElement('img');
-        img.src = mediaURL;
-        img.classList.add('img-fluid', 'img-thumbnail');
-        // Add click listener to load media into main image
-        img.addEventListener('click', () => {
-          document.getElementById('mainImage').src = mediaURL;
-        });
-        // Add hover listener to change main image on hover
-        img.addEventListener('mouseover', () => {
-          document.getElementById('mainImage').src = mediaURL;
-        });
-        imgContainer.appendChild(img);
-        relatedMediaContainer.appendChild(imgContainer);
+    `;
+
+    const relatedMediaContainers = this.querySelectorAll('.related-media');
+
+    imgSrc.forEach((mediaURL, index) => {
+      const img = this.ownerDocument.createElement('img');
+      img.src = mediaURL;
+      img.classList.add('img-fluid', 'img-thumbnail');
+
+      // Add click listener to load media into main image
+      img.addEventListener('click', () => {
+        const mainImage = this.querySelector('.main-image');
+        mainImage.src = mediaURL;
       });
-    }
-    // Load related media on page load
-    window.onload = loadRelatedMedia;
+
+      // Add hover listener to change main image on hover
+      img.addEventListener('mouseover', () => {
+        const mainImage = this.querySelector('.main-image');
+        mainImage.src = mediaURL;
+      });
+
+      // Append the 'img' element to the container selected based on the
+      // current index
+      relatedMediaContainers[index % relatedMediaContainers.length]
+          .appendChild(img);
+    });
   }
 }
 
